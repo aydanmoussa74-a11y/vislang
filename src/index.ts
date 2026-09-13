ce1dfaa37b8f8b6fac2054d31b869e484c91542a
Zayd Musa <aydanmoussa74@gmail.com>
2026-09-13 19:39:14 +0100

    feat: add library semantic diff engine

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 export { validateConstitution } from "./validate.ts";
 export { validateDtcgBoundary } from "./dtcg.ts";
+export { diffConstitutions } from "./diff.ts";
 export { parseJsonBytes, MAX_BYTES, MAX_DEPTH } from "./parse.ts";
 export type {
   Constitution,
@@ -7,3 +8,9 @@ export type {
   ValidationIssue,
   ValidationResult,
 } from "./types.ts";
+export type {
+  DiffChange,
+  DiffClass,
+  DiffOptions,
+  DiffResult,
+} from "./diff-types.ts";
